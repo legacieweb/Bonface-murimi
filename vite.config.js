155ab239c068cfce104179253c10d707310f1923
iyonicorp <hello@iyonicorp.com>
2026-02-05 02:54:01 +0300

    L

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://bonface-murimi.onrender.com',
+      '/api': 'https://bonfacemurimi.iyonicorp.com',
     },
   },
 })
